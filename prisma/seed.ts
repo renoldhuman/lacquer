8db092563cfe80ea26addebe8694f2befaf75dd0
Tyler Helmrich <25161748+renoldhuman@users.noreply.github.com>
2025-12-16 13:23:17 -0500

    Trying to resolve prisma related vercel issues

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../app/generated/prisma/client'
+import { PrismaClient } from '../generated/prisma/client'
 import { randomUUID } from 'crypto'
 
 const prisma = new PrismaClient()
